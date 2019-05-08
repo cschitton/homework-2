ca0691d73b59d0569d52d09f0591701cae26c938
Christian Schitton <dzenetaschitton@Dzenetas-MBP.home>
2019-05-08 16:48:11 +0200

    update test

--- test_homework_2.R
+++ test_homework_2.R
@@ -7,3 +7,8 @@ head(heights)
 
 h <- heights %>% filter(sex == "Male")
 head(h)
+
+h <- heights %>% filter(sex == "Male") %>%
+  summarize(avg = mean(height), sd = sd(height), se = sd(height) / sqrt(n()))
+
+h
